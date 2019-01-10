603de8882eae36aff642fb3e773efbf8951247e8
Katya Feldman <katyapinich@gmail.com>
2019-01-10 12:33:12 +0200

    Added updateCounterRegister method
    This method allows to update the value of IORegister[0].
    The value is reseted when reaching 0xFFFFFFFF.

--- BasysProject.X/io_registers_handler.c
+++ BasysProject.X/io_registers_handler.c
@@ -1,9 +1,32 @@
 #include "io_registers_handler.h"
 #include "commons.h"
 
+enum IORegisterIndex
+{
+    CounterRegister = 0,
+    LedsRegister = 1,
+    BtnCRegister = 2,
+    BtnDRegister = 3,
+    SSDRegister = 4
+};
+
 int isReadOnlyRegister(int registerIndex)
 {
-    return ((registerIndex == 0) || (registerIndex == 2) || (registerIndex == 3));
+    return ((registerIndex == CounterRegister) || 
+            (registerIndex == BtnCRegister) || 
+            (registerIndex == BtnDRegister));
+}
+
+int updateCounterRegister()
+{
+    if (IORegisters[CounterRegister] == 0xFFFFFFFF) 
+    {
+        IORegisters[CounterRegister] = 0;
+    }
+    else
+    {
+        IORegisters[CounterRegister] = IORegisters[0] + 1;
+    }
 }
 
 
