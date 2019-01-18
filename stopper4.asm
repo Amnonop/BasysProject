8007d11bb0f2c36a3d453c1338ba7b3f7b336acc
DESKTOP-C676D7P\Feldm <feldmanyuval@gmail.com>
2019-01-18 19:05:07 +0200

    Added code for LED0 change while in Stopper PAUSE

--- stopper4.asm
+++ stopper4.asm
@@ -3,7 +3,6 @@
 	in		$t2, $zero, $zero, $zero, 2		# : set $t2 = IORegister[2] - BUTNC state
 	add		$s0, $zero, $zero, $zero, 25		# :###UPDATE AFTER PAUSE!
 	in		$s2, $zero, $zero, $zero, 0		# : set $s2 = IORegister[0]
-	in		$t1, $zero, $zero, $zero, 1		# : set $t1 = IORegister[1]
 	in		$t3, $zero, $zero, $zero, 3		# : set $t3 = IORegister[3] - BUTND state
 
 stopper:
@@ -31,7 +30,6 @@ update_timer:
 	branch	$zero, $t1, $t2, 1, up_mins			#
 	and $t1, $at, $a0, $zero, 0x50				#
 	branch	$zero, $t1, $a0, 1, up_ten_mins		#
-	in	$a0, $zero, $zero, $zero, 3				# : set $t3 = IORegister[3] - BUTND state
 	branch $zero, $zero, $zero, $zero, stopper	#
 
 up_sec:
@@ -80,28 +78,30 @@ reset_clock:
 	branch	$zero, $zero, $zero, $zero, stopper		#
 
 pause_1:
-	in			$t2, 		$zero, 	$zero, 	$zero, 	2								# update current counter of BTNC
-	branch	$zero,	$zero,	$zero,	0,			pause_mode			# enter PAUSE mode
-	#in	$at, $zero, $zero, $zero, 2		# : (update) set $at = IORegister[2]
-	#add	$gp, $zero, $zero, $zero, 1		# :
-	#out	$gp, $zero, $zero, $zero, 1		# : set IORegister[1] = 1 means LD0 is on
-	#add $gp, $zero, $zero, $zero, 160	# : set $a2 = 160 = 32*5  (5 sec) #stalling for time delay between in $t2 and in	$a0
-	#in	$a1, $zero, $zero, $zero, 0		# : set $a1 = IORegister[0]
-	#sub	$s0, $a1, $t1, $zero, 0			# : $s0 = $a1 - $t1 (time difference)
-			#in	$v0, $zero, $zero, $zero, 3		# : set $a1 = IORegister[3] -> BTND
-			#branch	$zero, $t3, $v0, 1, BTND	# : if BTND changed
-			#branch	$zero, $s0, $gp, 2, led_change		# : if $s0 > 160
-	#branch	$zero, $at, $a0, 1, pause_2			# : break loop when $a0 != $at means IORegister[2]
-	#branch $zero, $zero, $zero, $zero, pause_1	# :jump back to pause_1
+	in		$t2, 	$zero, 	$zero, 	$zero, 	2					# update current counter of BTNC
+	in		$a0,	$zero,	$zero,	$zero,	1					# $a0 = IORegister[1]
+	add		$a0,	$a0,	$zero,	$zero,	1					# $a0 = 1
+	out		$a0,	$zero,	$zero,	$zero,	1					# IORegister[1] = $a0 (turn LED0 on)
+	branch	$zero,	$zero,	$zero,	0,		pause_mode			# enter PAUSE mode
 
 pause_mode:
-	in			$a0, 		$zero, 	$zero, 	$zero, 	2								# $a0 = IORegister[2]
-	branch	$zero,	$t2, 		$a0, 		1, 			exit_pause			# if BTNC state changed, exit PAUSE mode
-	branch	$zero,	$zero,	$zero,	0,			pause_mode			# continue PAUSE mode
+	in		$t1,	$zero,	$zero,	$zero,	0					# $t0 = IORegister[0]
+	in		$a0, 	$zero, 	$zero, 	$zero, 	2					# $a0 = IORegister[2]
+	branch	$zero,	$t2, 	$a0, 	1, 		exit_pause			# if BTNC state changed, exit PAUSE mode
+	in		$a0,	$zero,	$zero,	$zero,	3					# $a0 = IORegister[3]
+	branch	$zero,	$t3,	$a0,	1,		BTND				# if BTND state changed, initialize the display
+	add		$a0,	$zero,	$zero,	$zero,	160					# $a0 = 160 (approximatly 5 seconds)
+	in		$a1,	$zero,	$zero,	$zero,	0					# $a1 = IORegister[0]
+	sub		$a1,	$a1,	$t1,	$zero,	0					# $a1 = $a1 - $t1
+	branch	$zero,	$a1,	$a0,	2,		led_change			# if 5 seconds elapsed, turn off LED0
+	branch	$zero,	$zero,	$zero,	0,		pause_mode			# continue PAUSE mode
 
 exit_pause:
-	in			$t2, 		$zero, 	$zero, 	$zero, 	2								# update current counter of BTNC
-	branch	$zero,	$zero,	$zero,	0,			stopper					# enter PAUSE mode
+	in		$t2, 	$zero, 	$zero, 	$zero, 	2					# update current counter of BTNC
+	in		$a0,	$zero,	$zero,	$zero,	1					# $a0 = IORegister[1]
+	and		$a0,	$a0,	$zero,	$zero,	0XFF0				# turn off only last bit
+	out		$a0,	$zero,	$zero,	$zero,	1					# IORegister[1] = $a0 (turn LED0 off)
+	branch	$zero,	$zero,	$zero,	0,		stopper				# enter PAUSE mode
 
 BTND:
 	in	$t3, $zero, $zero, $zero, 3		# : update $t3 to new BTND state
@@ -119,11 +119,22 @@ BTND_2:
 	branch $zero, $zero, $zero, $zero, reset_clock	# :
 
 led_change:
-	in	$t1, $zero, $zero, $zero, 1			# : set $t1 = IORegister[1]
-	add $t1, $zero, $zero, $zero, 1			# : set $t1++ to change LED 0
-	and $t1, $t1, $t1, $zero, 1			    # : set $t1 = ($t1) & ($t1) & (0b...00001) mask all leds except for LD 0
-	out	$t1, $zero, $zero, $zero, 1			# : change LED register
-	branch $zero, $zero, $zero, $zero, pause_1	# : jump back to pause_1
+	in		$a0,	$zero,	$zero,	$zero,	1					# $a0 = IORegister[1]
+	and		$a0,	$a0,	$a0,	$zero,	0x001				# turn off last bit, to turn off LED0
+	branch	$zero,	$a0,	$zero,	0,		led_on				# if the led was off, turn it on				
+	branch	$zero,	$zero,	$zero,	0,		led_off				# else the led was on, turn it off
+
+led_on:
+	in		$a0,	$zero,	$zero,	$zero,	1					# $a0 = IORegister[1]
+	add		$a0,	$a0,	$zero,	$zero,	1					# $a0 = 1
+	out		$a0,	$zero,	$zero,	$zero,	1					# IORegister[1] = $a0
+	branch	$zero,	$zero,	$zero,	0,		pause_mode			# return to PAUSE mode
+
+led_off:
+	in		$a0,	$zero,	$zero,	$zero,	1					# $a0 = IORegister[1]
+	and		$a0,	$a0,	$zero,	$zero,	0XFF0				# turn off only last bit
+	out		$a0,	$zero,	$zero,	$zero,	1					# IORegister[1] = $a0
+	branch	$zero,	$zero,	$zero,	0,		pause_mode			# return to PAUSE mode
 
 halt:
 	sub $zero, $zero, $zero, $zero, 0		#
